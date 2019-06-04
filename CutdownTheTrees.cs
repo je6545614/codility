64f6f13eea9707bd145477a42c1b45e6e6594e37
je6545614 <jezanakub@hotmail.com>
2019-06-04 10:13:47 +0700

    Add files via upload

---
+++ CutdownTheTrees.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutdownTheTrees
+{
+    class CutdownTheTrees
+    {
+
+        public static int[] A = { 3, 4, 5, 4 }; //return 2
+        public static int[] B = { 4, 5, 2, 3, 4 }; //return 0
+        public static int[] C = { 1, 2, 3, 4, 5, 6, 7 }; //return 7
+
+        static void Main(string[] args)
+        {
+            int[] intUse = C;
+            List<int> intList = intUse.ToList();
+            int waysCan = 0;
+            for (int i = 0; i < intList.Count; i++)
+            {
+                List<int> tempList = intUse.ToList();
+                tempList.RemoveAt(i);
+                for (int x = 1; x < tempList.Count; x++)
+                {
+                    if (tempList[x - 1] <= tempList[x])
+                    {
+                        if ((x + 1) == tempList.Count)
+                        {
+                            waysCan += 1;
+                        }
+                    }
+                    else { break; }
+                }
+            }
+
+            Console.WriteLine("Ways to cutdown the trees : " + waysCan);
+
+        }
+    }
+}

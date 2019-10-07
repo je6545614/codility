b00aa016efb38a8e409da609df290ac5395116c6
je6545614 <jezanakub@hotmail.com>
2019-10-07 17:18:04 +0700

    Update Nesting.cs

--- Nesting.cs
+++ Nesting.cs
@@ -2,7 +2,7 @@ using System;
 
 class Solution {
     public int solution(string S) {
-       
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
         int l = S.Length;
         int count=0;
         
@@ -10,8 +10,10 @@ class Solution {
         
         foreach (char c in S)
         {
+            
             if (c == '(') count++;
             else count--;
+            if(count <0) return 0;//CORRECTING ABOVE ERROR!
         }
         if (count ==0 && S[0]=='(' && S[l-1]==')') return 1;
         else return 0;

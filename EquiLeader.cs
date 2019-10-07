5844f2d660b1cff6bad053929c4d6ed8e3e81b49
je6545614 <jezanakub@hotmail.com>
2019-10-07 17:24:47 +0700

    Update EquiLeader.cs

--- EquiLeader.cs
+++ EquiLeader.cs
@@ -1,63 +1,68 @@
-public static int solution(int[] A)
-{
-    int leader = 0;
-    int size = 0;
-    int leaderCount = 0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    // find the leader (this algorithm to find the leader in O(N) is obtained from the Leader lesson)
-    for (int i = 0; i < A.Length; i++)
-    {
-        if (size == 0)
+class Solution {
+    public int solution(int[] A) {
+        
+        int leader = 0;
+        int size = 0;
+        int leaderCount = 0;
+
+        for (int i = 0; i < A.Length; i++)
         {
-            size++;
-            leader = A[i];
-        }
-        else
-        {
-            if (A[i] == leader)
+            if (size == 0)
             {
                 size++;
+                leader = A[i];
             }
             else
             {
-                size--;
+                if (A[i] == leader)
+                {
+                    size++;
+                }
+                else
+                {
+                    size--;
+                }
             }
         }
-    }
 
-    if (size == 0)
-    {
-        return 0;
-    }
-
-    leaderCount = A.Count(i => i == leader);
-
-    if (leaderCount <= A.Length / 2)
-    {
-        return 0;
-    }
-
-    int equiCount = 0;
-
-    int leftSideLeaders = 0;
-    int rightSideLeaders = 0;
-    for (int i = 0; i < A.Length; i++)
-    {
-        if (A[i] == leader)
+        if (size == 0)
         {
-            leftSideLeaders++;
+            return 0;
         }
 
-        rightSideLeaders = leaderCount - leftSideLeaders;
+        leaderCount = A.Count(i => i == leader);
 
-        bool leftSideHasLeader = leftSideLeaders > (i + 1) / 2;
-        bool rightSideHasLeader = rightSideLeaders > (A.Length - (i + 1)) / 2;
-
-        if (leftSideHasLeader && rightSideHasLeader)
+        if (leaderCount <= A.Length / 2)
         {
-            equiCount++;
+            return 0;
         }
-    }
 
-    return equiCount;
+        int equiCount = 0;
+
+        int leftSideLeaders = 0;
+        int rightSideLeaders = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (A[i] == leader)
+            {
+                leftSideLeaders++;
+            }
+
+            rightSideLeaders = leaderCount - leftSideLeaders;
+
+            bool leftSideHasLeader = leftSideLeaders > (i + 1) / 2;
+            bool rightSideHasLeader = rightSideLeaders > (A.Length - (i + 1)) / 2;
+
+            if (leftSideHasLeader && rightSideHasLeader)
+            {
+                equiCount++;
+            }
+        }
+
+        return equiCount;
+    }
 }

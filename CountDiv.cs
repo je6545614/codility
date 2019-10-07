2324914a92f4df88d5b96e509e1c4e5b0bda53f3
je6545614 <jezanakub@hotmail.com>
2019-10-07 16:48:23 +0700

    Create CountDiv.cs

---
+++ CountDiv.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Solution {
+    public int solution(int A, int B, int K) {
+    
+        int result = 0;
+
+        if (A%K == 0) {
+          result = ((B-A)/K)+1;
+        } else {
+          result = (B/K - ((A/K)+1))+1;
+        }
+
+        return result;
+    }
+}

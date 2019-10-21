7f902b1228a123228c6dbde8cc1087eb87fb7661
je6545614 <jezanakub@hotmail.com>
2019-10-21 17:30:38 +0700

    Update MissingInteger.cs

--- MissingInteger.cs
+++ MissingInteger.cs
@@ -1,28 +1,33 @@
+using System;
+using System.Linq;
+
 class Solution {
+    
     const int maxSize = 100000;
-      
-    public int solution(int[] A)
-    {
+    
+    public int solution(int[] A) {
         int[] counter = new int[maxSize];
-        
         Array.Sort(A);
-
-        foreach (int number in A)
-        {
-            if (number > 0 && number <= maxSize)
-            {
+        int[] S = Distinct(A);
+        
+        foreach(int number in S){
+            if(number > 0 && number <= maxSize){
                 counter[number - 1] = 1;
-            }               
+            }
         }
-
-        for (int i = 0; i < maxSize; i++)
-        {
-            if (counter[i] == 0)                
-            {
+        
+        for(int i = 0; i < maxSize; i++){
+            if(counter[i] == 0){
                 return i + 1;
             }
         }
-
+        
         return maxSize + 1;
+        
+    }
+    
+    public int[] Distinct(int[] handles)
+    {
+        return handles.ToList().Distinct().ToArray();
     }
 }

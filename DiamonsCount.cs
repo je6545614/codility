b6dbf10482266fb3b3828a4e228435cb0bd1b065
je6545614 <jezanakub@hotmail.com>
2019-11-05 12:12:11 +0700

    Update DiamonsCount.cs

--- DiamonsCount.cs
+++ DiamonsCount.cs
@@ -6,43 +6,35 @@ class Solution
 {
     public int solution(int[] X, int[] Y)
     {
-        int chkX = X.Length, chkY = Y.Length;
-        if (chkX < 4 || chkX > 1500 || chkY < 4 || chkY > 1500)
-        {
-            return 0;
-        }
+        List<int> list_X = new List<int>(X);
+        List<int> list_Y = new List<int>(Y);
 
-        List<Int32> list_X = new List<Int32>(X);
-        List<Int32> list_Y = new List<Int32>(Y);
+        Dictionary<int, List<int>> x_dict = new Dictionary<int, List<int>>();
+        Dictionary<int, List<int>> y_dict = new Dictionary<int, List<int>>();
 
-        Dictionary<Int16, List<Int16>> x_dict = new Dictionary<Int16, List<Int16>>();
-        Dictionary<Int16, List<Int16>> y_dict = new Dictionary<Int16, List<Int16>>();
         var results = list_X.Zip(list_Y, (x, y) => new { x, y });
 
         foreach (var data in results)
         {
-            Int16 dataX = Convert.ToInt16(data.x);
-            Int16 dataY = Convert.ToInt16(data.y);
-
-            if (x_dict.Keys.Contains(dataX))
+            if (x_dict.Keys.Contains(data.x))
             {
-                x_dict[dataX].Add(dataY);
+                x_dict[data.x].Add(data.y);
             }
             else
             {
-                x_dict.Add(dataX, new List<Int16> { dataY });
+                x_dict.Add(data.x, new List<int> { data.y });
             }
 
-            if (y_dict.Keys.Contains(dataY))
+            if (y_dict.Keys.Contains(data.y))
             {
-                y_dict[dataY].Add(dataX);
+                y_dict[data.y].Add(data.x);
             }
             else
             {
-                y_dict.Add(dataY, new List<Int16> { dataX });
+                y_dict.Add(data.y, new List<int> { data.x });
             }
         }
-        
+
         if (x_dict.Count <= 2 || y_dict.Count <= 2)
         {
             return 0;
@@ -52,7 +44,7 @@ class Solution
 
         foreach (var i in x_dict)
         {
-            Dictionary<Int16, List<Int16>> point_count = new Dictionary<Int16, List<Int16>>();
+            Dictionary<int, List<int>> point_count = new Dictionary<int, List<int>>();
             point_count.Add(i.Key, i.Value);
             if (point_count[i.Key].Count > 1)
             {
@@ -60,23 +52,23 @@ class Solution
                 {
                     for (int t = o + 1; t < point_count[i.Key].Count; t++)
                     {
-                        Int16 y_sum = Convert.ToInt16(point_count[i.Key][o] + point_count[i.Key][t]);
+                        int y_sum = point_count[i.Key][o] + point_count[i.Key][t];
 
                         if ((y_sum % 2) == 0)
                         {
-                            Int16 y_value = Convert.ToInt16(y_sum / 2);
+                            int y_value = y_sum / 2;
 
                             if (y_dict.Keys.Contains(y_value))
                             {
-                                Dictionary<int, List<Int16>> y_point = new Dictionary<int, List<Int16>>();
+                                Dictionary<int, List<int>> y_point = new Dictionary<int, List<int>>();
                                 y_point.Add(y_value, y_dict[y_value]);
 
                                 if (y_point[y_value].Count > 1)
                                 {
-                                    List<Int16> sort_y_point = new List<Int16>(y_point[y_value].ToList());
+                                    var sort_y_point = y_point[y_value].ToList();
                                     sort_y_point.Sort();
-                                    Int16 min_num = sort_y_point[0];
-                                    Int16 max_num = sort_y_point[sort_y_point.Count - 1];
+                                    int min_num = sort_y_point[0];
+                                    int max_num = sort_y_point[sort_y_point.Count - 1];
 
                                     if (min_num < i.Key && i.Key < max_num)
                                     {
@@ -84,7 +76,7 @@ class Solution
                                         {
                                             if (j < i.Key)
                                             {
-                                                if (sort_y_point.Contains(Convert.ToInt16((2 * i.Key) - j)))
+                                                if (sort_y_point.Contains((2 * i.Key) - j))
                                                 {
                                                     count += 1;
                                                 }
